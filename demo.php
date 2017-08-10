a866b5bbdf47e91bcb4a8dd0492a6a115c5c79b4
jiejiezongheti <446914128@qq.com>
2017-08-10 19:49:02 +0800

    demo文件的curl测试

--- demo.php
+++ demo.php
@@ -1,12 +1,23 @@
 <?php
-$ch = curl_init(); 
+/*$ch = curl_init(); 
 $c_url = 'http://www.fxwin01.com'; 
 $c_url_data = "product_&type=1"; 
 curl_setopt($ch, CURLOPT_URL,$c_url); 
-/*curl_setopt($ch, CURLOPT_POST, 1); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-curl_setopt($ch, CURLOPT_POSTFIELDS, $c_url_data); */
-echo $result = curl_exec($ch);
+curl_setopt($ch, CURLOPT_POST, 1); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($ch, CURLOPT_POSTFIELDS, $c_url_data); 
+$result = curl_exec($ch);
 curl_close ($ch); 
-unset($ch); 
+echo $result;
+unset($ch); */
+// create a new curl resource 
+$ch = curl_init(); 
+// set URL and other appropriate options 
+curl_setopt($ch, CURLOPT_URL, 'http://www.baidu.com');
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+// grab URL, and print 
+curl_exec($ch);
+curl_close ($ch);
+unset($ch);
 ?>
