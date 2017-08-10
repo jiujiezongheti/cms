c52ca3278043f65b80903182f74458b6411c3fd6
jiejiezongheti <446914128@qq.com>
2017-08-10 11:47:18 +0800

    curl test

--- demo.php
+++ demo.php
@@ -1,5 +1,12 @@
 <?php
-require_once dirname(__FILE__)."/init.inc.php";
-$vc = new ValidateCode();
-echo $vc->doimg();
+$ch = curl_init(); 
+$c_url = 'http://www.fxwin01.com'; 
+$c_url_data = "product_&type=1"; 
+curl_setopt($ch, CURLOPT_URL,$c_url); 
+/*curl_setopt($ch, CURLOPT_POST, 1); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($ch, CURLOPT_POSTFIELDS, $c_url_data); */
+echo $result = curl_exec($ch);
+curl_close ($ch); 
+unset($ch); 
 ?>
